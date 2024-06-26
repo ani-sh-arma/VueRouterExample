5d2f636cea3ca16afbe17794aa7892c17615658d
Anish <anisharma030@gmail.com>
2024-06-27 00:00:59 +0530

    Test commit 1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -51,9 +51,8 @@ const removeCity = () => {
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
       <div class="flex items-center gap-3">
-        <i class="fa-solid fa-trash text-2xl text-white"></i>
+        <i class="fa-solid fa-sun text-2xl text-white"></i>
         <p class="text-2xl text-white">Local Weather</p>
-        Hello
       </div>
       <div class="flex gap-3 flex-1 justify-end">
         <i
