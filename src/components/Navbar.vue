3f9e40588731294d5c5b5b912f38056cc753da17
Anish <anisharma030@gmail.com>
2024-06-26 23:59:55 +0530

    Test commit 1

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -50,13 +50,11 @@ const removeCity = () => {
     <nav
       class="container flex flex-col sm:flex-row items-center gap-4 text-white py-6"
     >
-      <RouterLink>
-        <div class="flex items-center gap-3">
-          <i class="fa-solid fa-trash text-2xl text-white"></i>
-          <p class="text-2xl text-white">Local Weather</p>
-          Hello
-        </div>
-      </RouterLink>
+      <div class="flex items-center gap-3">
+        <i class="fa-solid fa-trash text-2xl text-white"></i>
+        <p class="text-2xl text-white">Local Weather</p>
+        Hello
+      </div>
       <div class="flex gap-3 flex-1 justify-end">
         <i
           class="fa-solid fa-circle-info text-xl hover:text-secondary duration-150 cursor-pointer"
